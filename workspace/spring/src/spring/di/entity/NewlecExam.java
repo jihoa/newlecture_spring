c1ca0cdfbcbe8de496bdec143a3df40c688a78f2
kimth007kim <kimth007kim@naver.com>
2021-04-01 02:24:27 +0900

    스프링 16강 특화된 @Component 어노테이션(@Controller/@Service/@Repository)

--- workspace/spring/src/spring/di/entity/NewlecExam.java
+++ workspace/spring/src/spring/di/entity/NewlecExam.java
@@ -1,11 +1,14 @@
 package spring.di.entity;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class NewlecExam implements Exam {
 
+	@Value("20")
 	private int kor;
+	@Value("30")
 	private int eng;
 	private int math;
 	private int com;
