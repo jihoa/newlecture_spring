239b2dd438c4a21b511b00729d23270352931c52
kimth007kim <kimth007kim@naver.com>
2021-04-08 03:11:53 +0900

    AOP 강좌 03강 - 순수 자바로 AOP 구현해보기

---
+++ workspace/spring/src/spring/aop/entity/NewlecExam.java
@@ -0,0 +1,89 @@
+package spring.aop.entity;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class NewlecExam implements Exam {
+
+	private int kor;
+	private int eng;
+	private int math;
+	private int com;
+	
+	public NewlecExam() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+	public NewlecExam(int kor, int eng, int math, int com) {
+		this.kor = kor;
+		this.eng = eng;
+		this.math = math;
+		this.com = com;
+	}
+
+
+
+	public int getKor() {
+		return kor;
+	}
+
+	public void setKor(int kor) {
+		this.kor = kor;
+	}
+
+	public int getEng() {
+		return eng;
+	}
+
+	public void setEng(int eng) {
+		this.eng = eng;
+	}
+
+	public int getMath() {
+		return math;
+	}
+
+	public void setMath(int math) {
+		this.math = math;
+	}
+
+	public int getCom() {
+		return com;
+	}
+
+	public void setCom(int com) {
+		this.com = com;
+	}
+
+	@Override
+	public int total() {
+//		long start=System.currentTimeMillis();
+		int result= kor+eng+math+com;
+		try {
+			Thread.sleep(200);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+//
+//		long end = System.currentTimeMillis();
+//		
+//		String message=(end-start)+"ms Ω√∞£¿Ã »Í∑∂¥Ÿ.";
+//		System.out.println(message);
+		return result;
+	}
+
+	@Override
+	public float avg() {
+		float result = total()/4.0f;
+		return result;
+	}
+
+	@Override
+	public String toString() {
+		return "NewlecExam [kor=" + kor + ", eng=" + eng + ", math=" + math + ", com=" + com + "]";
+	}
+
+}
