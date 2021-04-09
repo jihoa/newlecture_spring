63c1f7a3560e68d6dc90090ea286f0a33aa87bc6
kimth007kim <kimth007kim@naver.com>
2021-04-09 20:26:26 +0900

    AOP 강좌 07강 - Point Cut(Weaving, Join Point)

--- workspace/spring/src/spring/aop/advice/LogAfterReturningAdvice.java
+++ workspace/spring/src/spring/aop/advice/LogAfterReturningAdvice.java
@@ -10,7 +10,7 @@ public class LogAfterReturningAdvice implements AfterReturningAdvice {
 	public void afterReturning(Object returnValue, Method method, Object[] args, Object target) throws Throwable {
 		// 반환값을 가지고 할것이 있으면해라, 필요없으면 안써도된다.
 		System.out.println("returnValue:"+returnValue+", method:"+method.getName());
-		
+	
 	}
 
 }
