d71ae9d502137d3d6b776bd0b60898cfeee169d6
kimth007kim <kimth007kim@naver.com>
2021-04-27 22:52:30 +0900

    Spring MVC (스프링 웹 MVC) 강의 19 - Root 페이지들을 위한 Layout 페이지 만들기

--- springworkspace/webprj/src/main/java/com/newlecture/web/controller/IndexController.java
+++ springworkspace/webprj/src/main/java/com/newlecture/web/controller/IndexController.java
@@ -10,13 +10,12 @@ import org.springframework.web.servlet.mvc.Controller;
 public class IndexController implements Controller {
 
 	@Override
-	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception { //SpringFramework에 웹이라는 것을 확인하고 import하도록하자
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception { 
 		// TODO Auto-generated method stub
-		System.out.println("index controller");
-		ModelAndView mv = new ModelAndView("index");				
-		mv.addObject("data","Hello Spring MVC~");		//객체를 만든다. data,Hello Spring MVC를 담는다.
-//		mv.setViewName("/WEB-INF/view/index.jsp");					//set해서 view에대한 정보를담는다. index.jsp
-		return mv;		//반환하는것은 무엇이냐 Dispatcher기능은 빠졌고 스프링이 제공하고있으니까  디스패처를 하기위한 모델엔 ModelandView를 제공해 주는것(담아서 주는것)이다.
+		ModelAndView mv = new ModelAndView("root.index");				
+		mv.addObject("data","Hello Spring MVC~");
+		//mv.setViewName("/WEB-INF/view/index.jsp");				
+		return mv;	
 	}		
 
 }
