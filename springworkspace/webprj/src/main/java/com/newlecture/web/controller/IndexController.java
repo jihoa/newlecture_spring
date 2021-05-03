d86de5cae66943e38602f5493272ac7f41ccc391
kimth007kim <kimth007kim@naver.com>
2021-05-03 18:29:46 +0900

    Spring MVC (스프링 웹 MVC) 강의 27 - Annotation으로 URL 매핑하기

--- springworkspace/webprj/src/main/java/com/newlecture/web/controller/IndexController.java
+++ springworkspace/webprj/src/main/java/com/newlecture/web/controller/IndexController.java
@@ -1,21 +1,24 @@
 package com.newlecture.web.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
  
+@Controller
 
-public class IndexController implements Controller {
+public class IndexController{
+	@RequestMapping("/index")
+	public void aaaa() {
+		System.out.println("asdfsdf");
+	}
+//public class IndexController implements Controller {
 
-	@Override
-	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception { 
-		// TODO Auto-generated method stub
-		ModelAndView mv = new ModelAndView("root.index");				
-		mv.addObject("data","Hello Spring MVC~");
-		//mv.setViewName("/WEB-INF/view/index.jsp");				
-		return mv;	
-	}		
+//	@Override
+//	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception { 
+//		// TODO Auto-generated method stub
+//		ModelAndView mv = new ModelAndView("root.index");				
+//		mv.addObject("data","Hello Spring MVC~");
+//		//mv.setViewName("/WEB-INF/view/index.jsp");				
+//		return mv;	
+//	}		
 
 }
