7a5140ff1152e9f96b3b2dd830fa8ebdb4a8ade1
kimth007kim <kimth007kim@naver.com>
2021-05-24 20:30:45 +0900

    Spring MVC (스프링 웹 MVC) 강의 31 - 문서 출력방법 4가지 @ResponseBody

--- springworkspace/webprj/src/main/java/com/newlecture/web/controller/HomeController.java
+++ springworkspace/webprj/src/main/java/com/newlecture/web/controller/HomeController.java
@@ -1,15 +1,22 @@
 package com.newlecture.web.controller;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
  
 @Controller
 @RequestMapping("/")
 public class HomeController{
 	
 	@RequestMapping("index")
+	@ResponseBody
 	public String index() {
-		return "root.index";
+		return "Hello Index 1";
 	}
 	
 //public class IndexController implements Controller {
